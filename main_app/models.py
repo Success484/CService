ea3fbe48b564ec8f9a721ac5724b756db3d8a6fd
Success <successsimeon484@gmail.com>
2025-09-19 14:03:51 +0100

    feat: update

--- main_app/models.py
+++ main_app/models.py
@@ -7,23 +7,36 @@ def generate_tracking_number():
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=12))
 
 class Order(models.Model):
+    tracking_number = models.CharField(max_length=12, unique=True, default=generate_tracking_number, editable=False)
     PAYMENT_CHOICES = [
         ('pending', 'Pending'),
         ('paid', 'Paid'),
         ('failed', 'Failed'),
     ]
 
-    tracking_number = models.CharField(max_length=12, unique=True, default=generate_tracking_number, editable=False)
-    sender_name = models.CharField(max_length=100)
+    SERVICE_TYPE = [
+        ('sea', 'Sea Delivery'),
+        ('land', 'Land Delivery'),
+        ('Air', 'Land Delivery')
+    ]
+    # SHIPMENT DETAILS
+    quantity = models.IntegerField()
+    weight = models.FloatField(default=0.5)
+    service_type = models.CharField(max_length=200, choices=SERVICE_TYPE, default='land')
+    goods_description = models.TextField()
+    payment_status = models.CharField(max_length=10, choices=PAYMENT_CHOICES, default='pending')
+    # DESTINATION DETAILS
     receiver_name = models.CharField(max_length=100)
     receiver_email = models.EmailField(max_length=200)
+    receiver_address = models.CharField(max_length=255)
+    date_of_delivery = models.CharField(max_length=255)
+    # ORIGIN
+    sender_name = models.CharField(max_length=100)
     pickup_address = models.CharField(max_length=255)
-    delivery_address = models.CharField(max_length=255)
-    goods_description = models.TextField()
-    weight = models.FloatField(default=0.5)
-    payment_status = models.CharField(max_length=10, choices=PAYMENT_CHOICES, default='pending')
-    current_address = models.CharField(max_length=255, blank=True, null=True)
+    sender_address = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
+    delivery_address = models.CharField(max_length=255)
+    # current_address = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return f"{self.tracking_number} - {self.sender_name} to {self.receiver_name}"
